806d65179ccc00d1758b6d1cebe574c353c97f40
Rem0ld <p.lovergne@hotmail.fr>
2021-01-13 10:03:20 +0100

    creation of new files for more readalibility

---
+++ javacript/populate.js
@@ -0,0 +1,209 @@
+const list = [
+  {
+    websiteUrl: "https://terrasurabogados.cl",
+    codeUrl: "",
+    imageUrl:
+      "https://cdn.glitch.com/a56d7f44-3f97-4b8e-8e48-62256ce3c8d1%2FScreenshot_2020-08-26%20TerraSur%20Abogados%20-%20Asesoria%20Legal%20Lago%20LLanquihue.jpg?v=1598450959311",
+    altImage: "Terrasur abogados Landing Page",
+    stack: "full",
+  },
+  {
+    websiteUrl: "https://tribute-page-napol.glitch.me/",
+    codeUrl: "https://glitch.com/edit/#!/tribute-page-napol",
+    imageUrl:
+      "https://cdn.glitch.com/a56d7f44-3f97-4b8e-8e48-62256ce3c8d1%2Fnapoleon.jpg?v=1585843736025",
+    altImage: "Tribute Page about Napoléon Bonaparte",
+    stack: "front",
+  },
+  {
+    websiteUrl: "https://form-page.glitch.me/",
+    codeUrl: "https://glitch.com/edit/#!/form-page",
+    imageUrl:
+      "https://cdn.glitch.com/a56d7f44-3f97-4b8e-8e48-62256ce3c8d1%2Fform.jpg?v=1585843730316",
+    altImage: "Form webpage",
+    stack: "front",
+  },
+  {
+    websiteUrl: "https://landing-page-sc.glitch.me/",
+    codeUrl: "https://glitch.com/edit/#!/landing-page-sc",
+    imageUrl:
+      "https://cdn.glitch.com/a56d7f44-3f97-4b8e-8e48-62256ce3c8d1%2Fscootrix.png?v=1585843741728",
+    altImage: "Landing page Scootrix",
+    stack: "front",
+  },
+  {
+    websiteUrl: "https://doc-tech-rust.glitch.me/",
+    codeUrl: "https://glitch.com/edit/#!/doc-tech-rust",
+    imageUrl:
+      "https://cdn.glitch.com/e431f8fe-6864-4d58-a472-36bc6659a290%2FScreenshot_2020-08-26%20Documentation%20about%20Rust.png?v=1598450058798",
+    altImage: "Technical Documentation about Rust",
+    stack: "front",
+  },
+  {
+    websiteUrl: "https://codesandbox.io/s/electro-player-pl-5if05",
+    codeUrl: "",
+    imageUrl:
+      "https://cdn.glitch.com/a56d7f44-3f97-4b8e-8e48-62256ce3c8d1%2FScreenshot_2020-08-26%20React%20App.png?v=1598453981464",
+    altImage: "Electro Player",
+    stack: "front",
+  },
+  {
+    websiteUrl: "https://stackblitz.com/edit/markdown-previewpl?file=App.js",
+    codeUrl: "",
+    imageUrl:
+      "https://cdn.glitch.com/a56d7f44-3f97-4b8e-8e48-62256ce3c8d1%2FScreenshot_2020-08-26%20https%20markdown-previewpl%20stackblitz%20io.png?v=1598453976690",
+    altImage: "Mardown Preview",
+    stack: "front",
+  },
+  {
+    websiteUrl: "https://stackblitz.com/edit/quote-machinepl",
+    codeUrl: "",
+    imageUrl:
+      "https://cdn.glitch.com/a56d7f44-3f97-4b8e-8e48-62256ce3c8d1%2FScreenshot_2020-08-26%20https%20quote-machinepl%20stackblitz%20io.png?v=1598454084183",
+    altImage: "Drum Player",
+    stack: "front",
+  },
+  {
+    websiteUrl: "https://ld-page-frontend-mentor.vercel.app/",
+    codeUrl: "https://github.com/Rem0ld/ld-page-frontend-mentor",
+    imageUrl:
+      "https://raw.githubusercontent.com/Rem0ld/ld-page-frontend-mentor/master/images/Screenshot_2020-11-06%20Frontend%20Mentor%20Social%20proof%20section.png",
+    altImage: "Landing Page Frontend Mentor",
+    stack: "front",
+  },
+  {
+    websiteUrl: "https://four-card-feature-fd-mentor.vercel.app/",
+    codeUrl: "https://github.com/Rem0ld/four-card-feature-fd-mentor",
+    imageUrl:
+      "https://raw.githubusercontent.com/Rem0ld/four-card-feature-fd-mentor/master/images/Screenshot_2020-11-06%20Frontend%20Mentor%20Four%20card%20feature%20section.png",
+    altImage: "Four card Frontend Mentor",
+    stack: "front",
+  },
+  {
+    websiteUrl: "https://fylo-component-frontent-mentor.vercel.app/",
+    codeUrl: "https://github.com/Rem0ld/fylo-component-frontent-mentor",
+    imageUrl:
+      "https://raw.githubusercontent.com/Rem0ld/fylo-component-frontent-mentor/master/images/Screenshot_2020-11-06%20Frontend%20Mentor%20Fylo%20data%20storage%20component.png",
+    altImage: "Fylo component Frontend Mentor",
+    stack: "front",
+  },
+  {
+    websiteUrl: "https://art-prev-frontend-mentor.vercel.app/",
+    codeUrl: "https://github.com/Rem0ld/art-prev-frontend-mentor",
+    imageUrl:
+      "https://raw.githubusercontent.com/Rem0ld/art-prev-frontend-mentor/master/images/Screenshot_2020-11-06%20Frontend%20Mentor%20Article%20preview%20component.png",
+    altImage: "Article preview Frontend Mentor",
+    stack: "front",
+  },
+  {
+    websiteUrl: "https://faq-accor-frontend-mentor.vercel.app/",
+    codeUrl: "https://github.com/Rem0ld/faq-accor-frontend-mentor",
+    imageUrl:
+      "https://raw.githubusercontent.com/Rem0ld/faq-accor-frontend-mentor/master/images/Screenshot_2020-11-06%20Frontend%20Mentor%20FAQ%20Accordion%20Card.png",
+    altImage: "FAQ accordion Frontend Mentor",
+    stack: "front",
+  },
+  {
+    websiteUrl:
+      "https://coding-bootcamp-frontendmentor-git-master.rem0ld.vercel.app/",
+    codeUrl: "https://github.com/Rem0ld/coding-bootcamp-frontendmentor",
+    imageUrl:
+      "https://raw.githubusercontent.com/Rem0ld/coding-bootcamp-frontendmentor/master/images/Screenshot_2020-11-06%20Frontend%20Mentor%20Coding%20Bootcamp%20Testimonials%20Slider.png",
+    altImage: "Testimonial Frontend Mentor",
+    stack: "front",
+  },
+  {
+    websiteUrl: "https://media-dashboard-frontendmentor.vercel.app/",
+    codeUrl: "https://github.com/Rem0ld/media-dashboard-frontendmentor",
+    imageUrl:
+      "https://raw.githubusercontent.com/Rem0ld/media-dashboard-frontendmentor/master/images/Screenshot_2020-11-06%20Frontend%20Mentor%20%5BChallenge%20Name%20Here%5D.png",
+    altImage: "Social media dashboard Frontend Mentor",
+    stack: "front",
+  },
+  {
+    websiteUrl: "https://job-listing-frontendmentor.vercel.app/",
+    codeUrl: "https://github.com/Rem0ld/job-listing-frontendmentor",
+    imageUrl:
+      "https://raw.githubusercontent.com/Rem0ld/job-listing-frontendmentor/master/Screenshot_2020-11-06%20React%20App.png",
+    altImage: "Job listing Frontend Mentor",
+    stack: "front",
+  },
+  {
+    websiteUrl: "https://rest-api-country-frontend-mentor.vercel.app/",
+    codeUrl: "https://github.com/Rem0ld/rest-api-country-frontend-mentor",
+    imageUrl:
+      "https://raw.githubusercontent.com/Rem0ld/rest-api-country-frontend-mentor/master/Screenshot_2020-11-06%20Rest%20API%20countries.png",
+    altImage: "Rest API Country Frontend Mentor",
+    stack: "front",
+  },
+];
+
+function createCard(project) {
+  // First let's find the places I wanna generate my cards
+  const front = document.querySelector(".front-end .container-cards");
+  const full = document.querySelector(".full-stack .container-cards");
+
+  // Then I created each elements in order starting a the top
+  // (which is the card itself) to the deepest
+
+  const divCard = document.createElement("div");
+  divCard.className += "card";
+
+  const divHovering = document.createElement("div");
+  divHovering.className += "hovering";
+
+  const linkLeft = document.createElement("a");
+  linkLeft.href = project.websiteUrl;
+  linkLeft.target = "_blank";
+  linkLeft.rel = "noreferrer noopener";
+
+  const divHoveringLeft = document.createElement("div");
+  divHoveringLeft.className += "hovering-left";
+
+  const viewPage = document.createElement("p");
+  viewPage.textContent = "View Page";
+
+  if (project.codeUrl) {
+    const linkRight = document.createElement("a");
+    linkRight.href = project.codeUrl;
+    linkRight.target = "_blank";
+    linkRight.rel = "noreferrer noopener";
+
+    const divHoveringRight = document.createElement("div");
+    divHoveringRight.className += "hovering-right";
+
+    const viewCode = document.createElement("p");
+    viewCode.textContent = "View Code";
+
+    divHoveringRight.appendChild(viewCode);
+    linkRight.append(divHoveringRight);
+    divHovering.append(linkRight);
+  }
+
+  divHoveringLeft.appendChild(viewPage);
+  linkLeft.append(divHoveringLeft);
+  divHovering.append(linkLeft);
+
+  divCard.append(divHovering);
+
+  const divImage = document.createElement("div");
+  divImage.className += "card__container==img";
+
+  const img = document.createElement("img");
+  img.className = "card--img";
+  img.src = project.imageUrl;
+  img.alt = project.altImage;
+
+  divImage.appendChild(img);
+  divCard.append(divImage);
+
+  project.stack === "front"
+    ? front.appendChild(divCard)
+    : full.appendChild(divCard);
+}
+
+window.onload = function () {
+  list.forEach((project) => {
+    createCard(project);
+  });
+};
